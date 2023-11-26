992aa13c01294c7a6bebc2270666c217707efddf
beratkirik <beratkirik@hotmail.com>
2023-11-26 22:09:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Import necessary libraries
 import streamlit as st
-
+import math
 
 # Page configuration
 st.set_page_config(page_title="Stock Futures Pricing App", page_icon="📈")
@@ -8,24 +8,24 @@ st.set_page_config(page_title="Stock Futures Pricing App", page_icon="📈")
 # Title
 st.title("Stock Futures Pricing App")
 
+# A python function that calculates the value of a futures contract
+
+def FuturePrice(spotPrice, benefit, cost, r, t):
+    return (spotPrice - benefit + cost)*math.pow(1 + r, t);
+
 # Sidebar for user input
-symbol = st.sidebar.text_input("Enter Stock Symbol", "AAPL")
-expiration_date = st.sidebar.date_input("Select Expiration Date", value=None)
-strike_price = st.sidebar.number_input("Enter Strike Price", value=0.0)
-option_type = st.sidebar.radio("Select Option Type", ("Call", "Put"))
 
-# Fetch stock data
-stock_data = st.sidebar.number_input("Enter Stock Date", value=0.0)
-stock_price = st.sidebar.number_input("Enter Stock Price", value=0.0)
 
-# Calculate option price (you may need a more sophisticated pricing model)
-if option_type == "Call":
-    option_price = max(stock_price - strike_price, 0)
-else:
-    option_price = max(strike_price - stock_price, 0)
+symbol = st.sidebar.text_input("Enter Stock Symbol", "GARAN")
+benefit = st.sidebar.number_input("Dividend", value=0.0)
+miscCharges = st.sidebar.number_input("Cost", value=0.0);
+t = st.sidebar.number_input("Vadeye Kalan Gün Sayısı", value=0.0)
+r = st.sidebar.number_input("Vadeye Kalan Gün Sayısı", value=0.40);
+spotPrice = st.sidebar.number_input("Enter Stock Price", value=0.0)
+ratePerday      = r/365;
+
+contractPrice   = FuturePrice(spotPrice, benefit, miscCharges, r, t);
 
 # Display results
 st.write(f"Stock Price: ${stock_price}")
-st.write(f"Option Price: ${option_price}")
-
-# Additional features can be added based on your requirements
+st.write(f"Futures Price: ${contractPrice}")
