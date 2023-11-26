6fb8b3c7148750a2aed92c127fa63dfa86f635c7
beratkirik <beratkirik@hotmail.com>
2023-11-26 22:48:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,13 +14,16 @@ def FuturePrice(spotPrice, benefit, cost, r, t):
     return (spotPrice - benefit + cost)*math.pow(1 + r, t);
 
 # Sidebar for user input
+
+
 symbol = st.sidebar.text_input("Enter Stock Symbol", "GARAN");
 benefit = st.sidebar.number_input("Dividend", value=0.0);
 miscCharges = st.sidebar.number_input("Cost", value=0.0);
-t = st.sidebar.number_input("Vadeye Kalan Gün Sayısı", value=0.0);
-r = st.sidebar.number_input("Vadeye Kalan Gün Sayısı", value=0.40);
+t = st.sidebar.number_input("Days to Maturity", value=0.0);
+r = st.sidebar.number_input("Rate %", value=40.00);
 spotPrice = st.sidebar.number_input("Enter Stock Price", value=0.0);
-ratePerday      = r/365;
+rPercentage = r/100
+ratePerday      = rPercentage/365;
 
 contractPrice   = FuturePrice(spotPrice, benefit, miscCharges, ratePerday, t);
 
